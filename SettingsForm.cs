b981048f11efebe7bb7ea24a3edcb4cc66df5efb
Bryan Rykowski <90996715+BryanRykowski@users.noreply.github.co>
2020-05-19 22:04:42 -0400

    Added TabControl and Tabs.

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -12,11 +12,34 @@ namespace DimTray
 {
     public partial class SettingsForm : Form
     {
+        TabPage MonitorsTab;
+        TabPage ProfilesTab;
+        TabPage OptionsTab;
+        TabControl TabControl1;
+
         DTmonitors monitors = new DTmonitors();
 
         public SettingsForm()
         {
             InitializeComponent();
+
+            MonitorsTab = new TabPage("Monitors");
+            ProfilesTab = new TabPage("Profiles");
+            OptionsTab = new TabPage("Options");
+
+            TabControl1 = new TabControl();
+            TabControl1.Controls.AddRange( new Control[]{MonitorsTab, ProfilesTab, OptionsTab });
+            TabControl1.MinimumSize = new Size(640, 360);
+
+            this.Controls.Add(TabControl1);
+
+            Rectangle dimensions = this.RectangleToScreen(this.ClientRectangle);
+            int titleHeight = dimensions.Top - this.Top;
+
+            SuspendLayout();
+            Size = new Size(TabControl1.Width + 16, titleHeight + TabControl1.Height + 8);
+            ResumeLayout();
+
             refreshForm();
         }
 
