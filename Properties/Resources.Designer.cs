725f533e40ff3d0bbfb18a438cc40c54533d3d56
Bryan Rykowski <90996715+BryanRykowski@users.noreply.github.com>
2021-09-20 01:55:51 -0400

    Remove old icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace DimTray.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon TestIcon {
-            get {
-                object obj = ResourceManager.GetObject("TestIcon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
